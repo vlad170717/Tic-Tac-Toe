3ec4e487a1a0b2466aaf9b8ebb84c3e2cc291f42
Vlad <muravskyv@gmail.com>
2020-06-05 18:17:13 +0300

    tictactoe

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter cells: ");
-        String game = sc.nextLine();
+        String game = "         ";
         
 
         char[][] gameField = new char[4][4];
@@ -22,61 +21,46 @@ public class Main {
 
         printGameField(gameField);
 
-        System.out.println("Enter the coordinates: ");
-        String coords = sc.nextLine();
+        boolean goodMove;
+        int row;
+        int col;
+        char currentMove = 'X';
+        do {
+            goodMove = false;
+            do {
+                System.out.println("Enter the coordinates:");
+                String move = sc.nextLine();
 
-        int emptyFields = 9;
-        int xCount = 0;
-        int oCount = 0;
-        boolean threeXInRow;
-        boolean threeOInRow;
+                Scanner scMove = new Scanner(move);
+                if (!scMove.hasNextInt()) {
+                    System.out.println("You should enter numbers!");
+                    continue;
+                }
+                row = scMove.nextInt();
+                col = scMove.nextInt();
+                if (row < 1 || row > 3 || col < 1 || col > 3) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                    continue;
+                }
+                if (gameField[row][col] == 'X' || gameField[row][col] == 'O') {
+                    System.out.println("This cell is occupied! Choose another one!");
+                    continue;
+                }
+                gameField[row][col] = currentMove;
 
-        for (int i = 0; i <= 8; i++) {
-            switch (game.charAt(i)) {
-                case 'X': xCount++;
-                    emptyFields--;
-                    break;
-                case 'O': oCount++;
-                    emptyFields--;
-                    break;
+                goodMove = true;
+            } while (!goodMove);
+            printGameField(gameField);
+            if (currentMove == 'X') {
+                currentMove = 'O';
+            } else {
+                currentMove = 'X';
             }
-        }
-        threeXInRow = (game.charAt(0) == 'X' && game.charAt(1) == 'X' && game.charAt(2) == 'X')
-                   || (game.charAt(3) == 'X' && game.charAt(4) == 'X' && game.charAt(5) == 'X')
-                   || (game.charAt(6) == 'X' && game.charAt(7) == 'X' && game.charAt(8) == 'X')
-                   || (game.charAt(0) == 'X' && game.charAt(3) == 'X' && game.charAt(6) == 'X')
-                   || (game.charAt(1) == 'X' && game.charAt(4) == 'X' && game.charAt(7) == 'X')
-                   || (game.charAt(2) == 'X' && game.charAt(5) == 'X' && game.charAt(8) == 'X')
-                   || (game.charAt(0) == 'X' && game.charAt(4) == 'X' && game.charAt(8) == 'X')
-                   || (game.charAt(2) == 'X' && game.charAt(4) == 'X' && game.charAt(6) == 'X');
 
-        threeOInRow = (game.charAt(0) == 'O' && game.charAt(1) == 'O' && game.charAt(2) == 'O')
-                   || (game.charAt(3) == 'O' && game.charAt(4) == 'O' && game.charAt(5) == 'O')
-                   || (game.charAt(6) == 'O' && game.charAt(7) == 'O' && game.charAt(8) == 'O')
-                   || (game.charAt(0) == 'O' && game.charAt(3) == 'O' && game.charAt(6) == 'O')
-                   || (game.charAt(1) == 'O' && game.charAt(4) == 'O' && game.charAt(7) == 'O')
-                   || (game.charAt(2) == 'O' && game.charAt(5) == 'O' && game.charAt(8) == 'O')
-                   || (game.charAt(0) == 'O' && game.charAt(4) == 'O' && game.charAt(8) == 'O')
-                   || (game.charAt(2) == 'O' && game.charAt(4) == 'O' && game.charAt(6) == 'O');
+        } while (!gameIsOver(gameField));
 
-//        System.out.println(emptyFields);
-//        System.out.println(xCount);
-//        System.out.println(oCount);
-//        System.out.println(threeXInRow);
-//        System.out.println(threeOInRow);
 
-        if (((xCount - oCount) > 1 || (xCount - oCount) < -1 ) ||
-                (threeXInRow && threeOInRow)) {
-            System.out.println("Impossible");
-        } else if (emptyFields > 0 && !threeXInRow && !threeOInRow) {
-            System.out.println("Game not finished");
-        } else if (emptyFields == 0 && !threeXInRow && !threeOInRow) {
-            System.out.println("Draw");
-        } else if (threeXInRow) {
-            System.out.println("X wins");
-        } else {
-            System.out.println("O wins");
-        }
+
     }
 
     private static void printGameField(char[][] gameField) {
@@ -85,5 +69,54 @@ public class Main {
         System.out.println("| " + gameField[1][2] + " " + gameField[2][2] + " " + gameField[3][2] + " |");
         System.out.println("| " + gameField[1][1] + " " + gameField[2][1] + " " + gameField[3][1] + " |");
         System.out.println("---------");
+    }
+
+    private static boolean gameIsOver(char[][] gameField) {
+        int emptyFields = 9;
+        int xCount = 0;
+        int oCount = 0;
+        boolean threeXInRow;
+        boolean threeOInRow;
+
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                switch (gameField[i][j]) {
+                    case 'X': xCount++;
+                        emptyFields--;
+                        break;
+                    case 'O': oCount++;
+                        emptyFields--;
+                        break;
+                }
+            }
+        }
+        threeXInRow = checkThreeInRow('X', gameField);
+
+        threeOInRow = checkThreeInRow('O', gameField);
+
+        if (((xCount - oCount) > 1 || (xCount - oCount) < -1 ) ||
+                (threeXInRow && threeOInRow)) {
+        } else if (emptyFields == 0 && !threeXInRow && !threeOInRow) {
+            System.out.println("Draw");
+            return true;
+        } else if (threeXInRow) {
+            System.out.println("X wins");
+            return true;
+        } else if (threeOInRow) {
+            System.out.println("O wins");
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean checkThreeInRow(char Toe, char[][] gameField) {
+        return (gameField[1][1] == Toe && gameField[1][2] == Toe && gameField[1][3] == Toe)
+            || (gameField[2][1] == Toe && gameField[2][2] == Toe && gameField[2][3] == Toe)
+            || (gameField[3][1] == Toe && gameField[3][2] == Toe && gameField[3][3] == Toe)
+            || (gameField[1][1] == Toe && gameField[2][1] == Toe && gameField[3][1] == Toe)
+            || (gameField[1][2] == Toe && gameField[2][2] == Toe && gameField[3][2] == Toe)
+            || (gameField[1][3] == Toe && gameField[2][3] == Toe && gameField[3][3] == Toe)
+            || (gameField[1][1] == Toe && gameField[2][2] == Toe && gameField[3][3] == Toe)
+            || (gameField[3][1] == Toe && gameField[2][2] == Toe && gameField[1][3] == Toe);
     }
 }
